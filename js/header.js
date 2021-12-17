60350bd0239f79833c4a4e9742eaec7b2899cde6
Anumide <alabasamuelayomide3@gmail.com>
2021-12-17 22:36:14 +0100

    added the back to top animation

---
+++ js/header.js
@@ -0,0 +1,10 @@
+let backToTop = document.querySelector('.back-to-top')
+
+window.addEventListener('scroll', (e) => {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTop.style.transform = 'translateX(0px)'
+        backToTop.style.opacity = '1'
+    }else{
+        backToTop.style.transform = 'translateX(150px)'   
+    }
+})
