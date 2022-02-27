298a27a2c6bbf107dcd48a151a4703598e4b340b
Anumide <alabasamuelayomide3@gmail.com>
2022-02-27 14:05:30 +0100

    functionality for click events

--- js/header.js
+++ js/header.js
@@ -1,5 +1,10 @@
 
 let backToTop = document.querySelector('.back-to-top')
+let cartbtn = document.querySelector('.cart')
+let cart = document.querySelector('.cart-parent')
+let mobileMenu = document.querySelector('.hamburger')
+let mobileMenuContent = document.querySelector('.mobile-menu-parent')
+let closeMenu = document.querySelector('.close-menu')
 
 
 // back to top 
@@ -11,3 +16,26 @@ window.addEventListener('scroll', (e) => {
         backToTop.style.transform = 'translateX(250px)'   
     }
 })
+
+// functionality for cart 
+cartbtn.addEventListener('click', () =>{
+    if(cart.classList.contains('close-cart')){
+        cart.classList.remove('hidden')
+        cart.classList.remove('close-cart')
+        cart.classList.add('open-cart')
+    } else {
+        cart.classList.add('close-cart')
+        cart.classList.remove('open-cart')
+    }
+})
+
+
+// functionality for displaying menubar
+mobileMenu.addEventListener('click', () => {
+    mobileMenuContent.style.display = "block"
+})
+
+closeMenu.addEventListener('click', () =>{
+    mobileMenuContent.style.display = "none"
+})
+
