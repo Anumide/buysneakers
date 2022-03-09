ccd5cb1307bdd21a3856adf261309f0ca98d5361
Anumide <alabasamuelayomide3@gmail.com>
2022-03-09 03:34:10 +0100

    collection page done

--- js/header.js
+++ js/header.js
@@ -5,7 +5,7 @@ let cart = document.querySelector('.cart-parent')
 let mobileMenu = document.querySelector('.hamburger')
 let mobileMenuContent = document.querySelector('.mobile-menu-parent')
 let closeMenu = document.querySelector('.close-menu')
-
+let cartImage = document.querySelector('.cart svg')
 
 // back to top 
 window.addEventListener('scroll', (e) => {
@@ -23,9 +23,11 @@ cartbtn.addEventListener('click', () =>{
         cart.classList.remove('hidden')
         cart.classList.remove('close-cart')
         cart.classList.add('open-cart')
+        cartImage.style.fill = '#000'
     } else {
         cart.classList.add('close-cart')
         cart.classList.remove('open-cart')
+        cartImage.style.fill = '#69707D'
     }
 })
 
@@ -52,3 +54,24 @@ mobileMenuContent.addEventListener('click', () => {
     }, 600)
     document.body.style.overflowY = 'auto'
 })
+
+// functionality for adding and removing from cart 
+let cartParent = document.querySelector('.cart-parent'),
+    cartEmptyText = document.querySelector('.empty-cart-text'),
+    cartCheckOutBtn = document.querySelector('.cart-content-checkout-btn'),
+    cartContent = document.querySelector('.cart_content'),
+    numOfProduct = document.querySelector('.cart-number')
+    
+
+    
+
+if(cartContent.childElementCount >= 1){
+    cartEmptyText.style.display = 'none'
+    cartCheckOutBtn.style.display = 'block'
+    numOfProduct.innerContent = cartContent.childElementCount;
+} else{
+    cartParent.classList.toggle('height')
+    cartEmptyText.style.display = 'block'
+    cartCheckOutBtn.style.display = 'none'
+}
+    
